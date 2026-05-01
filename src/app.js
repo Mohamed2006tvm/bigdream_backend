13964ac5e8d0537f03412edb851cd523782c1357
Mohamed2006tvm <mohamed2006tvm@gmail.com>
2026-05-01 10:16:47 +0530

    Completed

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ const allowedOrigins = [
       'http://localhost:5173',
       'http://localhost:5174',
       'https://mybigdream.vercel.app',
+      'https://www.mydreamsurprise.com',
+      'https://mydreamsurprise.com',
       env.frontendUrl,
       'http://127.0.0.1:5173',
       'http://127.0.0.1:5174',
